8f6fd945a82a00f8175bcc84152e7630354c3792
Mike Blakley <m.blakley@icloud.com>
2019-10-08 18:51:20 -0400

    GET, POST, DELETE routes implemented with basic validation and logging

---
+++ src/store.js
@@ -0,0 +1,18 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: "He-Man",
+    url: "www.he-man.com",
+    description: "lived in a castle and rode a big cat",
+    rating: 3
+  },
+  {
+    id: 2,
+    title: "GI Joe",
+    url: "www.gijoe.com",
+    description: "not for the weak or faint of heart",
+    rating: 5
+  }
+]
+
+module.exports = { bookmarks }
